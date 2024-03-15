62e4322c630478fb4e15c47203d536664e0add8f
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2024-03-15 07:18:15 +0700

    feat: finish project

--- lib/utils/compress_image.dart
+++ lib/utils/compress_image.dart
@@ -1,9 +1,5 @@
-import 'dart:developer';
 import 'dart:typed_data';
-
 import 'package:flutter_image_compress/flutter_image_compress.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:image/image.dart' as img;
 
 Future<List<int>> compressImage({required XFile imageFile}) async {
   int quality = 100;
@@ -19,29 +15,11 @@ Future<List<int>> compressImage({required XFile imageFile}) async {
     if (compressedImageBytes!.length <= maxFileSize) {
       break;
     }
-
-    // Decrease the quality for further compression attempts
     quality -= 5;
 
     if (quality <= 0) {
-      // Quality reduction reached the minimum; break the loop
       break;
     }
   }
   return compressedImageBytes;
-
-  // final bytes = await file.readAsBytes();
-  // int imageLength = bytes.length;
-  // if (imageLength < 1000000) return bytes;
-  // final img.Image image = img.decodeImage(Uint8List.fromList(bytes))!;
-  // int compressQuality = 100;
-  // int length = imageLength;
-  // List<int> newByte = [];
-
-  // do {
-  //   compressQuality -= 10;
-  //   newByte = img.encodeJpg(image, quality: compressQuality);
-  //   length = newByte.length;
-  // } while (length > 1000000);
-  // return newByte;
 }
